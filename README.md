4581a2d145d0b1a0b5efd54d8bf80f4409eeac41
Russel Mahmud <russel_1237@hotmail.com>
2022-01-06 11:13:52 +0600

    Add docker support for local development

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# openctf
-Open Capture The Flag Platform
+# OpenCTF
+Capture The Flag Open Platform
+
+
+## References
+
+1. [Django Docker](https://testdriven.io/blog/dockerizing-django-with-postgres-gunicorn-and-nginx/)
--- openctf_api/config/development.py
+++ openctf_api/config/development.py
@@ -1,5 +1,18 @@
-from openctf_api.config.settings import *
+import os
+
+from config.settings import *
 
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
+        'PORT': os.environ.get('POSTGRES_PORT', 5432),
+    }
+}
